c569bd2709f1a43364f5996cbfee42246618c373
Alexander Becker <alexander.becker@tum.de>
2025-03-13 10:54:04 +0100

    Refactor super_resolve.py

--- super_resolve.py
+++ super_resolve.py
@@ -17,34 +17,39 @@ VAR = jnp.array([.25, .25, .25])
 PATCH_SIZE = 256
 
 
-def process(source, model, params, target_shape, do_ensemble=True):
+def process_single(source, apply_encoder, apply_decoder, params, target_shape):
     t = jnp.float32((target_shape[0] / source.shape[1])**-2)[None]
     coords_nearest = jnp.asarray(make_grid(target_shape)[None])
+    source_up = interpolate_grid(coords_nearest, source[None])
+    source = jax.nn.standardize(source, mean=MEAN, variance=VAR)[None]
 
+    encoding = apply_encoder(params, source)
+    coords = jnp.asarray(make_grid(source_up.shape[1:3])[None])  # global sampling coords
+    out = jnp.full_like(source_up, jnp.nan, dtype=jnp.float32)
+
+    for h_min in range(0, coords.shape[1], PATCH_SIZE):
+        h_max = min(h_min + PATCH_SIZE, coords.shape[1])
+        for w_min in range(0, coords.shape[2], PATCH_SIZE):
+            # apply decoder with one patch of coordinates
+            w_max = min(w_min + PATCH_SIZE, coords.shape[2])
+            coords_patch = coords[:, h_min:h_max, w_min:w_max]
+            out_patch = apply_decoder(params, encoding, coords_patch, t)
+            out = out.at[:, h_min:h_max, w_min:w_max].set(out_patch)
+
+    out = out * jnp.sqrt(VAR)[None, None, None] + MEAN[None, None, None]
+    out += source_up
+    return out
+
+
+def process(source, model, params, target_shape, do_ensemble=True):
     apply_encoder = jit(model.apply_encoder)
     apply_decoder = jit(model.apply_decoder)
 
     outs = []
     for i_rot in range(4 if do_ensemble else 1):
         source_ = jnp.rot90(source, k=i_rot, axes=(-3, -2))
-        source_up_ = interpolate_grid(coords_nearest, source_[None])
-        source_ = jax.nn.standardize(source_, mean=MEAN, variance=VAR)[None]
-        
-        encoding = apply_encoder(params, source_)
-        coords = jnp.asarray(make_grid(source_up_.shape[1:3])[None])  # global target coords
-        out = jnp.full_like(source_up_, jnp.nan, dtype=jnp.float32)
-
-        for h_min in range(0, coords.shape[1], PATCH_SIZE):
-            h_max = min(h_min + PATCH_SIZE, coords.shape[1])
-            for w_min in range(0, coords.shape[2], PATCH_SIZE):
-                w_max = min(w_min + PATCH_SIZE, coords.shape[2])
-                # apply decoder with one patch of coordinates
-                coords_patch = coords[:, h_min:h_max, w_min:w_max]
-                out_patch = apply_decoder(params, encoding, coords_patch, t)
-                out = out.at[:, h_min:h_max, w_min:w_max].set(out_patch)
-
-        out = out * jnp.sqrt(VAR)[None, None, None] + MEAN[None, None, None]
-        out += source_up_
+        target_shape_ = tuple(reversed(target_shape)) if i_rot % 2 else target_shape
+        out = process_single(source_, apply_encoder, apply_decoder, params, target_shape_)
         outs.append(jnp.rot90(out, k=i_rot, axes=(-2, -3)))
 
     out = jnp.stack(outs).mean(0).clip(0., 1.)
